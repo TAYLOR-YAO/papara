2aeb79e27439c70a4fa90a94b8a6059e5350662f
Taylor Yao <tayloryao@taylors-mbp.attlocal.net>
2018-04-25 13:13:36 -0400

    images changed

--- assets/javascript/papara.js
+++ assets/javascript/papara.js
@@ -1,35 +1,34 @@
 
 var t_shirt = $(".t-shirt");
-var long_sleeves = $(".long-sleeves")
+var long_sleeves = $(".long-sleeves");
+var tanTops = $(".tankTop");
+var hoobies = $(".hoobie")
 var long_sleevesValue = long_sleeves.val();
 var t_shirtValue = t_shirt.val();
 var prices; 
-var addToCart = [];
+const addToCart = [];
 var order = [];
 var itemName;
-var sum = addToCart.reduce((acc, val) => acc + val, 0);
 
+// function cartValue(a){
+//     var sum = a.reduce((acc, val) => acc + val, 0);
+//     console.log(sum);
+//     $(".price").append(sum + " $");    
 
-
-
+// }
 
 
 $(document).ready(function(){
     $(".quantity").text(addToCart.length +" Items")
-    $(".price").text(sum + " $");
-    
-    
+   
     
     t_shirt.on("click", function(){
         console.log(this.value)
         addToCart.push(this.value);
         console.log(addToCart);
         $(".quantity").text(addToCart.length +" Items")
-        
-        sum = addToCart.reduce((acc, val) => acc + val, 0);
-        $(".price").text(sum + " $");
-        console.log("Sum: " + sum);
-        
+
+        // cartValue(addToCart);
 
         if(this.value = 25){
             itemName = "T-shirt";
@@ -44,14 +43,11 @@ $(document).ready(function(){
         console.log(this.value)
         addToCart.push(this.value);
         console.log(addToCart);
-        $(".quantity").text(addToCart.length +" Items")
+        $(".quantity").text(addToCart.length +" Items");
+        // cartValue(addToCart);
         
-        sum = addToCart.reduce((acc, val) => acc + val, 0);
-        $(".price").text(sum + " $");
-        console.log("Sum: " + sum);
-        
-
-        if(this.value = 45){
+       
+        if(this.value = 30){
             itemName = "Long sleeve";
             order.push(itemName)
             console.log("Order Items: " + order)
@@ -60,6 +56,38 @@ $(document).ready(function(){
         }
     
     });
+
+
+    hoobies.on("click", function(){
+        console.log(this.value)
+        addToCart.push(this.value);
+        console.log(addToCart);
+        $(".quantity").text(addToCart.length +" Items");
+        
+
+        if(this.value = 40){
+            itemName = "Hoobie";
+            order.push(itemName)
+            console.log("Order Items: " + order)            
+            console.log(itemName)
+        }
+        
+    });
+
+    tanTops.on("click", function(){
+        console.log(this.value)
+        addToCart.push(this.value);
+        console.log(addToCart);
+        $(".quantity").text(addToCart.length +" Items");
+        if(this.value = 20){
+            itemName = "Tank Top";
+            order.push(itemName)
+            console.log("Order Items: " + order)            
+            console.log(itemName)
+        }  
+    });
+
+    
     
     
     $("#payement").on("click", function(){
@@ -72,7 +100,7 @@ $(document).ready(function(){
 
     });
 
-
+    
 
 
 
